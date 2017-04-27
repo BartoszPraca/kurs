29ea2b2d1303013bd97da6829eec755756ac3b6c
bartosz.krzywicki <bartosz.krzywicki@ho-ltp-13278.Interlink.local>
2017-04-27 15:16:52 +0200

    Pierwsza aktualizacja - nowa linia

--- P11-Git/src/hello/HelloBartek.java
+++ P11-Git/src/hello/HelloBartek.java
@@ -5,6 +5,9 @@ public class HelloBartek {
     
     public static void main(String[] args) {
         System.out.println("Witaj Bartek");
+        
+        System.out.println("Nowa linia");
+        
     }
     
 }
